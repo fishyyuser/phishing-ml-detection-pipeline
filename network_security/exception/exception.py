af93f79d10284f922c319974275c17b60e9b0ba8
fishyyuser <pradybisht@gmail.com>
2025-10-27 14:11:33 +0530

    updated logger and exceptions

--- network_security/exception/exception.py
+++ network_security/exception/exception.py
@@ -1,18 +1,27 @@
 import sys
-from network_security.logging import logger
-class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details:sys):
-        self.error_message=error_message
-        _,_,exc_tb=error_details.exc_info()
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename
-    
-    def __str__(self):
-        return "Error occured in the python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name,self.lineno,str(self.error_message))
 
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.Error(e)
-        raise NetworkSecurityException(e,sys)
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message
+
+        # Extract traceback safely
+        _, _, exc_tb = error_detail.exc_info()
+        if exc_tb is not None:
+            self.lineno = exc_tb.tb_lineno
+            self.file_name = exc_tb.tb_frame.f_code.co_filename
+        else:
+            self.lineno = None
+            self.file_name = None
+
+        logger.error(self.__str__())
+
+    def __str__(self):
+        if self.lineno and self.file_name:
+            return (
+                f"Error occurred in Python script [{self.file_name}] "
+                f"at line [{self.lineno}] "
+                f"with message: {self.error_message}"
+            )
+        else:
+            return str(self.error_message)
--- network_security/logging/logger.py
+++ network_security/logging/logger.py
@@ -2,18 +2,26 @@ import logging
 import os
 from datetime import datetime
 
-# Create timestamped log folder
-LOG_FOLDER = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
-LOG_DIR = os.path.join(os.getcwd(), "logs", LOG_FOLDER)
+# Create global logs directory
+LOGS_BASE_DIR = os.path.join(os.getcwd(), "logs")
+os.makedirs(LOGS_BASE_DIR, exist_ok=True)
+
+# Create timestamped log folder only once per run
+RUN_TIMESTAMP = datetime.now().strftime("%m_%d_%Y_%H_%M_%S")
+LOG_DIR = os.path.join(LOGS_BASE_DIR, RUN_TIMESTAMP)
 os.makedirs(LOG_DIR, exist_ok=True)
 
-# Define full log file path
-LOG_FILE = f"{LOG_FOLDER}.log"
+# Define log file path
+LOG_FILE = f"{RUN_TIMESTAMP}.log"
 LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
-# Configure logging
+# Configure logging (only once)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    level=logging.INFO,
 )
+
+# Create logger instance for the package
+logger = logging.getLogger("network_security")
+logger.setLevel(logging.INFO)
