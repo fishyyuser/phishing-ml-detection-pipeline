38ed99a0522b23bebe2c051930b4b2fdf4feff50
fishyyuser <pradybisht@gmail.com>
2025-10-28 10:52:50 +0530

    Added Data Ingestion component

---
+++ main.py
@@ -0,0 +1,19 @@
+from network_security.components.data_ingestion import DataIngestion
+from network_security.exception import NetworkSecurityException
+from network_security.logging import logger
+from network_security.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
+
+import sys
+
+
+
+if __name__=="__main__":
+    try:
+        training_pipeline_config=TrainingPipelineConfig()
+        data_ingestion_config=DataIngestionConfig(training_pipeline_config)
+        data_ingestion=DataIngestion(data_ingestion_config)
+        logger.info("Initiated the Data Ingestion")
+        data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
+        print(data_ingestion_artifact)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
