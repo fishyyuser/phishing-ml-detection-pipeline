7fdcc688b45f707c5f179ce2c7a8a70d6ba7ae08
fishyyuser <pradybisht@gmail.com>
2025-10-30 13:15:37 +0530

    added model trainer entity config and artifacts

--- network_security/constant/training_pipeline/__init__.py
+++ network_security/constant/training_pipeline/__init__.py
@@ -13,6 +13,8 @@ FILE_NAME:str="raw.csv"
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
 SCHEMA_FILE_PATH:str=os.path.join("data_schema","schema.yaml")
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
 
 """
 Data Ingestion related constants
